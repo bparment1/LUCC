584fdf3366a9401878bda80d65fc397ef5fd3c8b
Benoit Parmentier <benoit.parmentier@gmail.com>
2011-06-06 10:45:00 -0400

    quick test part2

--- MODISQC_flags_processing.py
+++ MODISQC_flags_processing.py
@@ -12,6 +12,7 @@ inputf1='albedo_band_quality_1.rgf'
 api = win32com.client.Dispatch('idrisi32.IdrisiAPIServer')    # api is the handle for IDRISI api
 wd = api.GetWorkingDir();
 #wd = 'G:\Benoit_Backup\Filling\ALBEDO_MCD43B2\W_OUTPUT_11_tiles_MCD43B2_BRDF_Albedo_Band_Quality\\'
+band_nb = 7;
 
 f2 = open(wd+inputf1,'r') 
 listfiles2_wd = f2.readlines()  #Reading all the lines in the file
@@ -22,14 +23,15 @@ for i in range(1,nb_files+1):
         loop_number2 = loop_number2 + 1
         filename2= listfiles2_wd[i]
         idrisi_filename2= filename2.rstrip('\n') #Remove the LF character from the string
-        #MODISQC     
-        parameters ='3'+'*'+wd+idrisi_filename2+'.rst'+'*'+wd+idrisi_filename2+'_qc_1'+'.rst'+'*1'  #option 2 is an avl file with option 1 , count in cells in textfile '*.avl'
-        #parameters = '2*G:\Benoit_Backup\STA_PAPER2_02262011\W_ALASKA_2001_025_MCD43B2_REPROJECTED_BRDF_ALBEDO_BAND_QUALITY.rst*G:\Benoit_Backup\STA_PAPER2_02262011\Albedo_quality_band_qc\W_ALASKA_2001_025_BRDF_ALBEDO_BAND_QUALITY_QC_5.rst*1'
+        for b in range(1,band_nb+1):
+                #MODISQC     
+                parameters ='3'+'*'+wd+idrisi_filename2+'.rst'+'*'+wd+idrisi_filename2+'_qc_'+str(b)+'.rst'+'*'+str(b)  #option 2 is an avl file with option 1 , count in cells in textfile '*.avl'
+                #parameters = '2*G:\Benoit_Backup\STA_PAPER2_02262011\W_ALASKA_2001_025_MCD43B2_REPROJECTED_BRDF_ALBEDO_BAND_QUALITY.rst*G:\Benoit_Backup\STA_PAPER2_02262011\Albedo_quality_band_qc\W_ALASKA_2001_025_BRDF_ALBEDO_BAND_QUALITY_QC_5.rst*1'
 
-        module = 'modisqc'
-        print('Running ' + module + ' module with parameters ' + parameters)
-        success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
-        if not success:
-               print('Error running ' + module + '!')
+                module = 'modisqc'
+                print('Running ' + module + ' module with parameters ' + parameters)
+                success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
+                if not success:
+                       print('Error running ' + module + '!')
 
        
