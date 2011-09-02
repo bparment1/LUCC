616c10b3659b52b30c20760eba65a185742dfc9c
Benoit Parmentier <benoit.parmentier@gmail.com>
2011-09-02 10:45:00 -0400

    adding randomization of segment order in the speudo image stat generation for chaincluster use

--- Segments_statistics.py
+++ Segments_statistics.py
@@ -1,4 +1,4 @@
-#This script calculate statistic for segments or polygons in an image. It also creates pseudo images based on segments ID and input bands.
+#This script calculates statistic for segments or polygons in an image. It also creates pseudo images based on segments ID and input bands.
 #The script is divided in two parts: the first part creates the rgf. series for as input for TSTATS while the second part runs the
 #TSTATS module.
 
@@ -11,12 +11,19 @@ import shutil
 
 #inputf1= 'Amplitudes_2001_2009_filling6__9_STA_Slopes.rgf' #bands information
 inputf1= 'Amplitudes_2001_2009_filling6__9_STA_Slopes_std.RGF'
+#inputf1= 'Intercepts_9_2001_2009_filling6__STA_amplitudes_TS_std.rgf'
+#inputf1= '9_intercepts_NDVI_LST_ALB_2001_2009.rgf'
 #inputf1= 'PCA_corr_Amp9_fill6__T-Mode_Comps.rgf'
 
-output_prefix= 'PCA_'
-wd1='H:\Benoit_Backup\Paper3_STA_07202011\Segments_classifications\\' #folder location of the groupfile and files
 
-inputf3='seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__30' #This is the segment level used or the feature definition image used
+output_prefix= 'rd1_STDV_'  #'INTERCEPTS' 
+#wd1='H:\Benoit_Backup\Paper3_STA_07202011\Segments_classifications\\' #folder location of the groupfile and files
+wd1= 'H:\Benoit_Backup\Paper3_STA_07202011\Segments_classifications\seg_classification_random\\'
+
+#inputf3='seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__30' #This is the segment level used or the feature definition image used
+inputf3='seg12_NDVI_LST_ALB_A0_A1_A2_TS_s_0_450__0' #This is the segment level used or the feature definition image used
+random = 1;  #If random is equal 1 then create a random pseudo image with ID at random places 
+
 #inputf3='ISODATA5'
 
 #inputf3= 'ID_burnt_unburnt_05232011'
@@ -58,13 +65,48 @@ success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the ou
 if not success:
     print('Error running ' + module + '!')
 
-#RUN THE RANK MODULE USING DESCENDING
-parameters = wd1+'ps_'+inputf3+'_initial.rst'+'*none*'+wd1+'ps_'+inputf3+'_initial_PIXID.rst'+'*D'   
-module = 'RANK'
+#RUN MODULE RANDOM
+
+parameters = wd1+'ps_'+inputf3+'_random.rst'+'*'+'1*2*'+wd1+'ps_'+inputf3+'_initial.rst'+'*1'
+module = 'random'
 print('Running ' + module + ' module with parameters ' + parameters)
 success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
 if not success:
     print('Error running ' + module + '!')
+
+#RUN STRETCH
+
+parameters = wd1+'ps_'+inputf3+'_random.rst'+'*'+wd1+'ps_'+inputf3+'_random_byte.rst'+'*1*min*max*N*256*0*255*unknown'
+module = 'stretch'
+print('Running ' + module + ' module with parameters ' + parameters)
+success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
+if not success:
+    print('Error running ' + module + '!')   
+
+###RUN MODULE STRECH
+##    
+###STRETCH  G:\Benoit_Backup\STA_PAPER2_02262011\ps_seg0_random.rst*G:\Benoit_Backup\STA_PAPER2_02262011\ps_seg0_random_byte.rst*1*min*max*N*256*0*255*unknown
+##RANK  G:\Benoit_Backup\STA_PAPER2_02262011\ps_seg0_random_byte.rst*none*G:\Benoit_Backup\STA_PAPER2_02262011\ps_seg0_random_rank.rst*A    
+
+if random==1:
+    
+    #RUN THE RANK MODULE USING DESCENDING
+    parameters = wd1+'ps_'+inputf3+'_random_byte.rst'+'*none*'+wd1+'ps_'+inputf3+'_initial_PIXID.rst'+'*D'     
+    module = 'RANK'
+    print('Running ' + module + ' module with parameters ' + parameters)
+    success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
+    if not success:
+        print('Error running ' + module + '!')
+        
+else:
+    
+    #RUN THE RANK MODULE USING DESCENDING
+    parameters = wd1+'ps_'+inputf3+'_initial.rst'+'*none*'+wd1+'ps_'+inputf3+'_initial_PIXID.rst'+'*D'   
+    module = 'RANK'
+    print('Running ' + module + ' module with parameters ' + parameters)
+    success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
+    if not success:
+        print('Error running ' + module + '!')
 
 #RUN MODULE EXTRACT
 parameters = wd1+'ps_'+inputf3+'_initial_PIXID.rst'+'*'+wd1+'ps_'+inputf3+'_initial_PIXID.rst'+'*'+'1*1*'+wd1+'ps_'+inputf3+'_initial_PIXID'+'.avl'
@@ -130,7 +172,7 @@ for i in range(1,nb_files+1,1):
         idrisi_filename1= filename1.rstrip('\n')
 
         #EXTRACT stat_n2 = '8' #SD summary
-        parameters = wd1+inputf3+'.rst'+'*'+wd1+idrisi_filename1+'.rst'+'*'+'1*'+stat_n2+'*'+wd1+inputf3+'_ID'+'_b_'+str(loop_number1)+'_SD.avl'
+        parameters = wd1+inputf3+'.rst'+'*'+wd1+idrisi_filename1+'.rst'+'*'+'1*'+stat_n2+'*'+wd1+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_SD.avl'
         module = 'extract'
         print('Running ' + module + ' module with parameters ' + parameters)
         success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
@@ -139,7 +181,7 @@ for i in range(1,nb_files+1,1):
 
 
         #EXTRACT stat_n1 = '4' #AVG summary
-        parameters = wd1+inputf3+'.rst'+'*'+wd1+idrisi_filename1+'.rst'+'*'+'1*'+stat_n1+'*'+wd1+inputf3+'_ID'+'_b_'+str(loop_number1)+'_avg.avl'
+        parameters = wd1+inputf3+'.rst'+'*'+wd1+idrisi_filename1+'.rst'+'*'+'1*'+stat_n1+'*'+wd1+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_avg.avl'
         module = 'extract'
         print('Running ' + module + ' module with parameters ' + parameters)
         success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
@@ -147,7 +189,7 @@ for i in range(1,nb_files+1,1):
             print('Error running ' + module + '!')
             
         #RUN ASSIGN
-        parameters = wd1+'ps_'+inputf3+'_PIXID'+'.rst'+'*'+wd1+'ps_'+inputf3+'_ID'+'_b_'+str(loop_number1)+'_SD.rst'+'*'+wd1+inputf3+'_ID'+'_b_'+str(loop_number1)+'_SD.avl'  
+        parameters = wd1+'ps_'+inputf3+'_PIXID'+'.rst'+'*'+wd1+'ps_'+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_SD.rst'+'*'+wd1+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_SD.avl'  
         module = 'assign'
         print('Running ' + module + ' module with parameters ' + parameters)
         success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
@@ -155,7 +197,7 @@ for i in range(1,nb_files+1,1):
             print('Error running ' + module + '!')
 
         #RUN ASSIGN
-        parameters = wd1+'ps_'+inputf3+'_PIXID'+'.rst'+'*'+wd1+'ps_'+inputf3+'_ID'+'_b_'+str(loop_number1)+'_avg.rst'+'*'+wd1+inputf3+'_ID'+'_b_'+str(loop_number1)+'_avg.avl'  
+        parameters = wd1+'ps_'+inputf3+'_PIXID'+'.rst'+'*'+wd1+'ps_'+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_avg.rst'+'*'+wd1+inputf3+'_ID_'+output_prefix+'_b_'+str(loop_number1)+'_avg.avl'  
         module = 'assign'
         print('Running ' + module + ' module with parameters ' + parameters)
         success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
@@ -164,8 +206,8 @@ for i in range(1,nb_files+1,1):
 
 #CREATING RGF FOR SD OF THE POLYGONS/SEGMENTS STATISTICS
             
-listfiles1_wd1 = glob.glob(wd1+'ps_'+inputf3+'_ID'+'_b_*_SD.rst')
-rgf_name_SD =wd1+'ps_'+inputf3+'_'+str(nb_files)+'_SD.rgf'
+listfiles1_wd1 = glob.glob(wd1+'ps_'+inputf3+'_ID_'+output_prefix+'_b_*_SD.rst')
+rgf_name_SD =wd1+'ps_'+inputf3+'_'+output_prefix+'_'+str(nb_files)+'_SD.rgf'
 
 f4 = open( rgf_name_SD,'w')
 #f4 = open( wd1+'ps_'+inputf3+'_'+str(nb_files)+'.rgf','w')
@@ -180,8 +222,8 @@ f4.close()
 
 #CREATING RGF FOR AVG OF THE POLYGONS/SEGMENTS STATISTICS
 
-listfiles1_wd2 = glob.glob(wd1+'ps_'+inputf3+'_ID'+'_b_*_avg.rst')
-rgf_name_avg =wd1+'ps_'+inputf3+'_'+str(nb_files)+'_avg.rgf'
+listfiles1_wd2 = glob.glob(wd1+'ps_'+inputf3+'_ID_'+output_prefix+'_b_*_avg.rst')
+rgf_name_avg =wd1+'ps_'+inputf3+'_'+output_prefix+'_'+str(nb_files)+'_avg.rgf'
 
 f5 = open( rgf_name_avg,'w')
 line0 = str(len(listfiles1_wd2))+'\n'
@@ -193,40 +235,47 @@ for line in listfiles1_wd2:
     f5.write(line) 
 f5.close()
 
-#PARAMETERS FOR RUN TSTATS
-#rgf_name =wd1+'ps_'+inputf3+'_'+str(nb_files)+'SD.rgf'
-outfilename =wd1+'ps_'+inputf3+'_'+str(nb_files)+'_SD'
-
-min_range =-1000
-max_range =1000000000000000
-background = -1000
-min_nb_obs =1
-
-#RUN TSTATS     
-parameters ='1'+'*'+rgf_name_SD+'*'+outfilename+'*'+str(min_range)+'*'+str(max_range)+'*'+str(background)+'*'+str(min_nb_obs)
-module = 'tstats'
-print('Running ' + module + ' module with parameters ' + parameters)
-success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
-if not success:
-       print('Error running ' + module + '!')
+if random==1:
+    os.rename(wd1+'ps_'+inputf3+'_PIXID'+'.rst',wd1+'ps_'+inputf3+output_prefix+'_PIXID'+'.rst')
+    os.rename(wd1+'ps_'+inputf3+'_PIXID'+'.rdc',wd1+'ps_'+inputf3+output_prefix+'_PIXID'+'.rdc')
 
 
-# ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__0_9_SD_Mean.rst
-#stat = api.Calc_Mean_and_SD(wd1+'ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__0_9_SD_Mean.rst','1','1')
 
-listfiles1_wd3= glob.glob(wd1+'ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__*_9_SD_Mean.rst')
+###PARAMETERS FOR RUN TSTATS
+###rgf_name =wd1+'ps_'+inputf3+'_'+str(nb_files)+'SD.rgf'
+##outfilename =wd1+'ps_'+inputf3+'_'+output_prefix+'_'+str(nb_files)+'_SD'
+##
+##min_range =-1000
+##max_range =1000000000000000
+##background = -1000
+##min_nb_obs =1
 
-rgf_name_SD_mean =wd1+'ps_'+inputf3+'_'+str(nb_files)+'_SD_mean.rgf'
-
-f6 = open( rgf_name_SD_mean,'w')
-line0 = str(len(listfiles1_wd3))+'\n'
-f6.write(line0)
-for line in listfiles1_wd2:
-    line = line[line.rfind('\\')+1:]
-    #line = line[line.rfind('\\')+1:-4]
-    line=line.replace('.rst','\n')
-    f6.write(line) 
-f6.close()
+###RUN TSTATS     
+##parameters ='1'+'*'+rgf_name_SD+'*'+outfilename+'*'+str(min_range)+'*'+str(max_range)+'*'+str(background)+'*'+str(min_nb_obs)
+##module = 'tstats'
+##print('Running ' + module + ' module with parameters ' + parameters)
+##success = api.RunModule(module, parameters, True, '', '', '', '', True)  #the output of api.runmodule is a value '1' if it is sucessful and '0' if not.
+##if not success:
+##       print('Error running ' + module + '!')
+##
+##
+### ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__0_9_SD_Mean.rst
+###stat = api.Calc_Mean_and_SD(wd1+'ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__0_9_SD_Mean.rst','1','1')
+##
+##listfiles1_wd3= glob.glob(wd1+'ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__*_9_SD_Mean.rst')
+##
+##
+##rgf_name_SD_mean =wd1+'ps_'+inputf3+'_'+str(nb_files)+'_SD_mean.rgf'
+##
+##f6 = open( rgf_name_SD_mean,'w')
+##line0 = str(len(listfiles1_wd3))+'\n'
+##f6.write(line0)
+##for line in listfiles1_wd2:
+##    line = line[line.rfind('\\')+1:]
+##    #line = line[line.rfind('\\')+1:-4]
+##    line=line.replace('.rst','\n')
+##    f6.write(line) 
+##f6.close()
 
 ##SCALAR  H:\Benoit_Backup\Paper3_STA_07202011\ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__309_Mean.rst*G:\Benoit_Backup\STA_PAPER2_02262011\ps_seg10_NDVI_LST_ALB_A0_A1_A2_TS_slope_scale_0_450__309_sum.rst*3*9               
 ##
